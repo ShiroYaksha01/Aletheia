35b7f78411665bafc413ffc370c672e19fffdd28
Ty <kitylim123@gmail.com>
2025-12-18 15:15:20 +0700

    complete ui & fix controller

---
+++ src/main/java/aletheia/project/Aletheia/controller/WebController.java
@@ -0,0 +1,28 @@
+package aletheia.project.Aletheia.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import aletheia.project.Aletheia.dto.LoginRequest;
+import aletheia.project.Aletheia.dto.RegisterRequest;
+
+@Controller
+public class WebController {
+    @GetMapping("/login")
+    public String loginPage(Model model) {
+        model.addAttribute("loginRequest", new LoginRequest());
+        return "auth/login"; // points to src/main/resources/templates/auth/login.html
+    }
+
+    @GetMapping("/register")
+    public String registerPage(Model model) {
+        model.addAttribute("registerRequest", new RegisterRequest());
+        return "auth/signup"; // points to src/main/resources/templates/auth/register.html
+    }
+
+    @GetMapping("/dashboard")
+    public String dashboard() {
+        return "pages/dashboard";
+    }
+}
--- src/main/java/aletheia/project/Aletheia/repository/UserRepository.java
+++ src/main/java/aletheia/project/Aletheia/repository/UserRepository.java
@@ -1,8 +1,10 @@
-package aletheia.project.Aletheia.security;
+package aletheia.project.Aletheia.repository;
 
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import aletheia.project.Aletheia.entity.UserEntity;
 
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
     Optional<UserEntity> findByEmail(String email);
